8a6cde7fca4f42513aa52e40fae88d4f58086a22
Vulcani <166513177+Guvulcani@users.noreply.github.com>
2024-06-06 09:51:39 -0300

    PROVA JS MUITO DIFICIL
    SOCORRO

---
+++ JS/index.js
@@ -0,0 +1,55 @@
+let nome = document.getElementById("Nome");
+let Resposta = document.getElementById("Resposta");
+function Apertar()
+{
+
+  Resposta.innerHTML = "OLÁ," + nome.value + ",TUDO BEM?";
+}
+//-----------------------------------------
+
+let centimetros = document.getElementById("centimetros")
+let abc = document.getElementById("abc")
+
+function converter()
+{
+    abc.innerHTML = parseInt(centimetros.value)/100
+
+}
+//------------------------------------------
+function ache()
+{
+  RESULTADO.innerHTML = parseFloat(numero1.value) >= (numero2.value)
+
+if (10 >= 11)
+  {
+      console.log("é maior")
+  }
+  else
+  {
+      console.log("é menor")
+  }
+}
+//-------------------------------
+var C = 0
+while(C <= 100)
+    {
+        console.log(C)
+        C = C + 1
+    }
+//------------------------------
+var a = 0
+while(a <= 50)
+    {
+        console.log(a)
+        a = a + 5
+    }
+//-----------------------------
+var b = 100
+while(b >= 0)
+    {
+        console.log(b)
+        b = b - 10
+    }
+
+    
+
